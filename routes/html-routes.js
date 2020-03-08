6ffd8c2decc5da39c7f0be51c8da87966e5f373b
Teresa Liu <kuas033@gmail.com>
2020-03-07 16:32:44 -0800

    try ajax call

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,20 +7,29 @@
 // Dependencies
 // =============================================================
 const path = require(`path`);
-
+const express = require(`express`);
+const router= express.Router();
 // Routes
 // =============================================================
-module.exports = app => {
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+// module.exports = app => {
+//   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-  app.get(`/`, (req, res) => {
-    res.sendFile(path.join(__dirname, `../public/index.html`));
-  });
+//   // index route loads view.html
+//   app.get(`/`, (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/sushi.html'));
+//   });
 
-  // cms route loads cms.html
-  app.get(`/sushi`, (req, res) => {
-    res.sendFile(path.join(__dirname, `../public/sushi.html`));
-  });
+//   // // cms route loads cms.html
+//   // app.get(`/sushi`, (req, res) => {
+//   //   res.sendFile(path.join(__dirname, `../public/sushi.html`));
+//   // });
 
-};
+// };
+
+
+// get route -> index
+router.get('/', (req, res) => {
+  res.render('index',{});
+});
+
+module.exports = router;
