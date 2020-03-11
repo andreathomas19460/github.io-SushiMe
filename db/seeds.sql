647927e1487702f576c40603de865219e340d9da
Andrea Thomas <ANDREA.THOMAS19460@GMAIL.COM>
2020-03-11 00:19:09 -0700

    deleted seeds.sql

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-USE sushi_db;
-
-INSERT INTO sushi(sushi_name, sushi_jap, image) VALUES
-('octupus', 'タコ', LOAD_FILE('C:\Users\andre_vely0h2\UW-Code-Boot-Camp\SushiMe-Project\github.io-SushiMe\public\assets\images'));
-
