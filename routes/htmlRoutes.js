150b47807a1c6bc4b5ee0d6b8a1b61c4ab3c9317
Teresa Liu <kuas033@gmail.com>
2020-03-12 20:14:00 -0700

    change html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,33 +3,26 @@ const db = require ('../models');
 module.exports = app =>{
     app.get ('/', function(req,res){
         res.render('index', {
-            style: 'style.css'
+            style: 'landingPage.css'
         })
     });
     app.get ('/home', function(req,res){
         res.render('home', {
-            style: 'home.css'
-        })
-    });
-    app.get ('/sushi', function(req,res){
-        db.Sushi.findAll({}).then(function(result){
-            const test = JSON.stringify(result)
-            console.log(`this is the object ${test}`)
-            const sushiNames = result.map((item) => {
-                return item.name_ja;
-            });
-            console.log(`These are the sushi names ${sushiNames}`);
-
-            res.render('sushi', {
-                style: 'sushi.css',
-                sushi: result
-            })
+            style: 'style.css'
         })
     });
 
+    app.get ('/sushiMe', function(req,res){
+        res.render('sushi', {
+            style: 'sushiMe.css',
+            
+        })
+    });
+     
     app.get ('/map', function(req,res){
         res.render('map', {
             style: 'map.css'
         })
     });
+
 }
