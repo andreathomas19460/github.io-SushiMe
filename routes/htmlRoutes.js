bcee026903ed4aa1113d966350eea1a19f0f398f
Teresa Liu <kuas033@gmail.com>
2020-03-13 20:47:14 -0700

    card success

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,37 +1,47 @@
-const db = require (`../models`);
-const fs = require('fs');
- 
-const sushiList = fs
-  .readdirSync(__dirname+"/../public/assets/images/sushi_db")
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file.slice(-4) === '.jpg');
-  })
-  .map(file => {
-    return file.split(".")[0]
-  })
+const db = require(`../models`);
 
-console.log(sushiList)
+const sushiList = [
+    ["blue_mackerel", "blue mackerel", "サバ"],
+    ["salmon", 'salmon', 'シャケ'],
+    ["tuna", 'tuna', 'マグロ'],
+    ["sea_urchin", 'sea urchin', 'ウニ'],
+    ["red_sea_bream", 'red sea bream', 'マダイ'],
+    ["octopus", 'octopus', 'タコ'],
+    ["yellowtail", 'yellowtail', 'ハマチ'],
+    ["shrimp", 'shrimp', 'エビ'],
+    ["puffer_fish", 'puffer fish', 'フグ'],
+    ["fluke", 'fluke', 'ヒラメ'],
+    ["sardine", 'sardine', 'イワシ'],
+    ["spanish_mackerel", 'spanish mackerel', 'サワラ'],
+    ["sea_bass", 'sea bass', 'スズキ'],
+    ["freshwater_eel",'freshwater eel', 'ウナギ'],
+    ["surf_clam", 'surf clam', 'ホッキガイ'],
+    ["oyster", 'oyster', 'カキ'],
+    ["crab", 'crab', 'カニ'],
+    ["salmon_roe", 'salmon roe', 'イクラ'],
+    ["flying_fish_roe", 'flying fish roe', 'トビコ'],
+]
 
-module.exports = app =>{
-    app.get ('/', function(req,res){
+module.exports = app => {
+    app.get('/', function (req, res) {
         res.render('index', {
             style: 'landingPage.css'
         })
     });
-    app.get ('/home', function(req,res){
+    app.get('/home', function (req, res) {
         res.render('home', {
             style: 'style.css'
         })
     });
 
-    app.get ('/sushiMe', function(req,res){
+    app.get('/sushiMe', function (req, res) {
         res.render('sushiMe', {
             style: 'sushiMe.css',
             sushiList: sushiList,
         })
     });
-     
-    app.get ('/map', function(req,res){
+
+    app.get('/map', function (req, res) {
         res.render('map', {
             style: 'map.css'
         })
